bd4a367b3f4fef7c14bf0d6fb077e0dc4500c2b6
= <=>
2015-02-28 01:08:47 -0300

    Creating test for training data class

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	version='0.1',
+	description='Implementation of some machine learning algorithms',
+	author='Lucas Moura',
+	author_email='nate@natereed.com',
+	packages=find_packages(),
+	install_requires=['numpy'],
+	test_suite="tests",
+	)
---
+++ tests/test_training_data.py
@@ -0,0 +1,77 @@
+import unittest
+
+from model.training_data import TrainingData
+from os import getcwd
+
+class TestTrainingData(unittest.TestCase):
+
+	def setUp(self):
+		self.trainingData = TrainingData()
+
+	def testLoadTrainingData(self):
+		
+		delimiter = ','
+		training_file = './tests/test_files/training_data.txt'
+		expected_value = 0;
+
+		return_value = self.trainingData.load_training_data(training_file,
+															delimiter)
+
+		self.assertEqual(0, return_value)
+
+		x_size = self.trainingData.x.shape;
+		num_rows_x = x_size[0];
+		expected_value = 5; 
+
+		self.assertEqual(num_rows_x, expected_value)
+		
+		num_columns_x = x_size[1];
+		expected_value = 1;
+		
+		self.assertEqual(num_columns_x, expected_value)
+
+		y_size = self.trainingData.y.shape;
+		num_rows_y = y_size[0]
+		expected_value = 5; 
+
+		self.assertEqual(num_rows_y, expected_value)
+		
+		num_columns_y = y_size[1];
+		expected_value = 1;
+		
+		self.assertEqual(num_columns_y, expected_value)
+
+	def testTrainingDataWithMoreInputs(self):
+
+		delimiter = ','
+		training_file = './tests/test_files/training_data_more_inputs.txt'
+		expected_value = 0;
+
+		return_value = self.trainingData.load_training_data(training_file,
+															delimiter)
+
+		self.assertEqual(0, return_value)
+
+		x_size = self.trainingData.x.shape;
+		num_rows_x = x_size[0];
+		expected_value = 6; 
+
+		self.assertEqual(num_rows_x, expected_value)
+		
+		num_columns_x = x_size[1];
+		expected_value = 4;
+		
+		self.assertEqual(num_columns_x, expected_value)
+
+		y_size = self.trainingData.y.shape;
+		num_rows_y = y_size[0]
+		expected_value = 6; 
+
+		self.assertEqual(num_rows_y, expected_value)
+		
+		num_columns_y = y_size[1];
+		expected_value = 1;
+		
+		self.assertEqual(num_columns_y, expected_value)
+
+
