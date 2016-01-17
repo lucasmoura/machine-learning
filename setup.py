a4b17e5e64dc420ec3cc0e31b86c31108d678f67
Lucas Moura <lucas.moura128@gmail.com>
2016-01-17 16:04:52 -0200

    Fix setup.py email and project name
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
+    name='machine_learning',
     version='0.1',
     description='Implementation of some machine learning algorithms',
     author='Lucas Moura',
-    author_email='nate@natereed.com',
+    author_email='lucas.moura128@gmail.com',
     packages=find_packages(),
     install_requires=['numpy'],
     test_suite="tests",
