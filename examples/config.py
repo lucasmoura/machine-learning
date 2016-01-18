a8df7be7f22f4991e707cc585bf959298210edb7
Lucas Moura <lucas.moura128@gmail.com>
2016-01-18 01:11:58 -0200

    Add Perceptron OR logic example
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- examples/config.py
+++ examples/config.py
@@ -6,3 +6,5 @@ sys.path.append(lib_path)
 
 REGRESSION_DATA_HOUSE = '../data/ex1data1.txt'
 REGRESSION_DATA_MULTIPLE_VARIABLE = '../data/ex1data2.txt'
+
+PERCEPTRON_DATA_OR = '../data/perceptron_or.txt'
