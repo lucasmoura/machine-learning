32caf1d848fd5526a20b1241df77740fb61e9569
Lucas Moura <lucas.moura128@gmail.com>
2016-01-18 01:11:01 -0200

    Add trainPerceptron implementation
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -40,7 +40,6 @@ class Perceptron():
         activation = dot(self.inputMatrix, self.weights)
         return where(activation > 0, 1, 0)
 
-    
     """
     Method used to update the weights of each output neuron based
     on the given results of a training step. The error can be calculated by
@@ -56,16 +55,28 @@ class Perceptron():
         xi = feature associated with the given weight being updated
 
     :param resultMatrix: A Nxn matrix that hold the obtained labels t for
-                        a certain training step.
+                         a certain training step.
     :param learningRate: The learning rate that will be used on the weight
-                        update.
+                         update.
     """
     def updateWeights(self, resultMatrix, learningRate):
         self.weights -= learningRate * dot(self.inputMatrix.T,
                                            resultMatrix - self.answerMatrix)
 
-    def trainPerceptron(self):
-        pass
+    """
+    Method used to train the perceptron algorithm. For each step, it first
+    calculate the feedForward answer for the given weights and the update the
+    weights given the received answer.
+
+    :param numSteps:     The number of training steps the algorithm will need
+                         to reach an optimal result.
+    :param learningRate: The learning rate that will be used on the weight
+                         update.
+    """
+    def trainPerceptron(self, numSteps, learningRate):
+        for step in range(numSteps):
+            resultMatrix = self.feedForward()
+            self.updateWeights(resultMatrix, learningRate)
 
     """
     This method will be used to initialize the weights
